b17efbdb8e77fc12322973b955e26c37b5e35cd4
Andy Chiang <andy10801@gmail.com>
2021-06-16 12:50:51 +0800

    p021_Merge Two Sorted Lists

---
+++ src/p021_MergeTwoSortedLists/Solution2.java
@@ -0,0 +1,74 @@
+package p021_MergeTwoSortedLists;
+
+public class Solution2 {
+
+	public static void main(String[] args) {
+		Solution2 sol = new Solution2();
+		ListNode l1 = null;
+		System.out.print("l1:");
+		sol.showLinkedList(l1);
+		ListNode l2 = null;
+		l2 = sol.addNode(l2, 4);
+		System.out.print("l2:");
+		sol.showLinkedList(l2);
+		System.out.println("\n-------------");
+		ListNode l3 = sol.mergeTwoLists(l1, l2);
+		System.out.println("l3:");
+		sol.showLinkedList(l3);
+	}
+
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		ListNode head = new ListNode(0, null), ptr = head;
+		while(l1 != null && l2 != null) {
+			if(l1.val > l2.val) {
+				ListNode node = new ListNode(l2.val, null);
+				ptr.next = node;
+				ptr = node;
+				l2 = l2.next;
+			}
+			else {
+				ListNode node = new ListNode(l1.val, null);
+				ptr.next = node;
+				ptr = node;
+				l1 = l1.next;
+			}
+		}
+		if(l1 == null) {
+			ptr.next = l2;
+		}
+		if(l2 == null) {
+			ptr.next = l1;
+		}
+		
+		return head.next;
+	}
+	
+	public ListNode addNode(ListNode head, int val) {
+		ListNode newNode = new ListNode(val, head);
+		return newNode;
+	}
+	
+	public void showLinkedList(ListNode head) {
+		for(ListNode lead = head; lead != null; lead = lead.next) {
+			System.out.print(lead.val+" ");
+        }
+		System.out.println();
+	}
+
+	class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+}
