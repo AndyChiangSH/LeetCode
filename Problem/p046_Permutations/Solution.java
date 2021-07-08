82ed1818c11b97c39cb8a89c47853a4653836cb4
Andy Chiang <andy10801@gmail.com>
2021-07-08 23:23:06 +0800

    p046_Permutations

---
+++ Problem/p046_Permutations/Solution.java
@@ -0,0 +1,11 @@
+package p046_Permutations;
+
+public class Solution {
+	public static void main(String[] args) {
+		
+	}
+	
+	public List<List<Integer>> permute(int[] nums) {
+        return null;
+    }
+}
