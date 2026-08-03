ea5d71347051e0437b5bd72b15368aff25f42e12
AndyChiangSH <andy10801@gmail.com>
2026-08-03 17:50:53 +0800

    p877: Solution2

---
+++ Problem/p877_Stone Game/Solution2.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def stoneGame(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
+        
+        return True
+    
+
+# Accepted
+# 46 / 46 testcases passed
